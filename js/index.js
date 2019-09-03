371ae5f2da4b6ea2af7e607b80293c7fd9995aaf
Francis Bulus <notchera@gmail.com>
2019-09-03 13:36:34 +0100

    added mouse leave and mouse enter event listeners for header image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+//#1 - onmouseenter listener for header image to be inverted
+const headerImg = document.querySelector("img");
+headerImg.onmouseenter = function () {
+    headerImg.style.filter = "invert(0.80)";
+}
+
+//#2 - onmouseleave listener for header image to be inverted
+headerImg.onmouseleave = function () {
+    headerImg.style.filter = ""
+}
+
+// //3 - onload listener for page to alert "Welcome" upon loading
+// document.body.onload = function () {
+//     alert("Welcome to Fun Bus, Minion!");
+// }
+
+//4
